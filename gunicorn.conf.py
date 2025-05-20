39ff22acb13515a519825a4d57ce6186b7c347d1
kubanurse <jabawookez99@gmail.com>
2025-05-20 15:06:41 +0200

    .

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,19 +2,16 @@ import multiprocessing
 
 # Server socket
 bind = "0.0.0.0:8000"
-backlog = 2048
 
 # Worker processes
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 4
 worker_class = 'gevent'
-worker_connections = 1000
 timeout = 30
-keepalive = 2
 
 # Logging
+loglevel = "debug"
 accesslog = "logs/access.log"
 errorlog = "logs/error.log"
-loglevel = "debug"
 
 # Process naming
 proc_name = 'chocomap'
@@ -22,9 +19,6 @@ proc_name = 'chocomap'
 # Server mechanics
 daemon = False
 pidfile = "gunicorn.pid"
-umask = 0
-user = None
-group = None
 
 # Server hooks
 def on_starting(server):
