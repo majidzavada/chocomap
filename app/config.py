b1823949ba6832db7057fcf7450f3410c79b2016
majidzavada <jabawookez99@gmail.com>
2025-05-23 14:10:34 +0200

    config.py edited

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ class Config:
     # Database configuration
     MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
     MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '3070')
     MYSQL_DB = os.environ.get('MYSQL_DB', 'chocomap')
     MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))
     
